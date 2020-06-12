d364f0651c22c43ee8d5721f5d9d3bf54e1b3932
HollosJ <hollosj99@gmail.com>
2020-06-12 12:21:54 +0100

    Updates

---
+++ precache-manifest.68cfd3c6672f672054858e0ec4f7d5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c9357c0262eda23425306bc714e1ab",
+    "url": "/react-countries/index.html"
+  },
+  {
+    "revision": "a6971f3b93784beb356a",
+    "url": "/react-countries/static/css/main.f6866239.chunk.css"
+  },
+  {
+    "revision": "3cfe7f76db12a11498fe",
+    "url": "/react-countries/static/js/2.42afcaf8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-countries/static/js/2.42afcaf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6971f3b93784beb356a",
+    "url": "/react-countries/static/js/main.2039510a.chunk.js"
+  },
+  {
+    "revision": "caffee5eed2f1cfa27ad",
+    "url": "/react-countries/static/js/runtime-main.bca96395.js"
+  }
+]);
